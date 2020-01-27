96c08405059aeba04ea0e14ddf63745684748405
permaficus <abukhalif2019@gmail.com>
2020-01-28 01:13:56 +0700

    Add else condition as instructed

--- Week 2/proxytia.js
+++ Week 2/proxytia.js
@@ -1,13 +1,14 @@
 proxytia = (name, role) => {
     if (!name) {return 'Nama harus diisi!'}
     if (!role) {return `Halo ${name}, Pilih peranmu untuk memulai game!`}
-    switch (role) {
-        case 'Ksatria': return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, kamu dapat menyerang dengan senjatamu!\n`;
-        case 'Tabib' : return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, kamu akan membantu temanmu yang terluka\n`;
-        default : return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, ciptakan keajaiban yang membantu kemenanganmu!\n`;
+    else {
+        switch (role) {
+            case 'Ksatria': return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, kamu dapat menyerang dengan senjatamu!\n`;
+            case 'Tabib' : return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, kamu akan membantu temanmu yang terluka\n`;
+            default : return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, ciptakan keajaiban yang membantu kemenanganmu!\n`;
+        }
     }
 }
-
 [
     proxytia('',''),
     proxytia('Mikael',''),
