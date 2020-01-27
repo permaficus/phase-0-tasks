86a1463f5a9d4c7c90a13ddee0ee4f96a58018d2
permaficus <abukhalif2019@gmail.com>
2020-01-28 01:06:59 +0700

    Adding new line for better reading in console.log

--- Week 2/proxytia.js
+++ Week 2/proxytia.js
@@ -2,9 +2,9 @@ proxytia = (name, role) => {
     if (!name) {return 'Nama harus diisi!'}
     if (!role) {return `Halo ${name}, Pilih peranmu untuk memulai game!`}
     switch (role) {
-        case 'Ksatria': return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, kamu dapat menyerang dengan senjatamu!`;
-        case 'Tabib' : return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, kamu akan membantu temanmu yang terluka`;
-        default : return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, ciptakan keajaiban yang membantu kemenanganmu!`;
+        case 'Ksatria': return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, kamu dapat menyerang dengan senjatamu!\n`;
+        case 'Tabib' : return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, kamu akan membantu temanmu yang terluka\n`;
+        default : return `Selamat datang di Dunia Proxytia, ${name}\nHalo ${role} ${name}, ciptakan keajaiban yang membantu kemenanganmu!\n`;
     }
 }
 
