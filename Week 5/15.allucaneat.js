044c3a331f97b18d7463e1d2b475c2b9362761fa
permaficus <abukhalif2019@gmail.com>
2020-02-04 23:16:59 +0700

    Fixing something

--- Week 5/15.allucaneat.js
+++ Week 5/15.allucaneat.js
@@ -1,8 +1,8 @@
-ayce = timer => {return (timer <=0) ? 0:ayce(timer - 15) + 1;}
+allYouCanEat = timer => {return (timer <=0) ? 0:allYouCanEat(timer - 15) + 1;}
   
 // TEST CASES
-console.log(ayce(66), 'Expected 5');
-console.log(ayce(100),'Expected 7');
-console.log(ayce(90),'Expected 6');
-console.log(ayce(10),'Expected 1');
-console.log(ayce(0),'Expected 0');
+console.log(allYouCanEat(66), 'Expected 5');
+console.log(allYouCanEat(100),'Expected 7');
+console.log(allYouCanEat(90),'Expected 6');
+console.log(allYouCanEat(10),'Expected 1');
+console.log(allYouCanEat(0),'Expected 0');
