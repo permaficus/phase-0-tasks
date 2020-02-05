42d82a3dc7b15cf7be7ed9140a8f4fa8df91946d
permaficus <abukhalif2019@gmail.com>
2020-02-05 20:32:00 +0700

    Adding naive solution

--- Week 4/4.findmodus.js
+++ Week 4/4.findmodus.js
@@ -1,12 +1,34 @@
  findModus = a => {
 
-    let obj = {};
+   // 1st SOLUTION
+   // let obj = {};
+   // for (let c=0;c<a.length;c++){obj[a[c]] = (obj[a[c]]||0)+1;}
+   // let result = Object.keys(obj).filter(el=>obj[el]>1).sort((prev,cur)=>obj[cur]-obj[prev]);
+   
+   // 2nd & Naive Solution
+   const sorting = n => {
+       let f = 0;
+       for (let min=0, max=0, c=0;c<n.length;c++){ if (n[c]>n[c+1]) { min = n[c+1];
+           max = n[c]; n[c]=min; n[c+1]=max; f++}}
+       return (f==0) ? n:sorting(n);
+   }
 
-    for (let c=0;c<a.length;c++){obj[a[c]] = (obj[a[c]]||0)+1;}
-    let result = Object.keys(obj).filter(el=>obj[el]>1).sort((prev,cur)=>obj[cur]-obj[prev]);
-    
-    return (result.length < 1 || Object.values(obj)==a.length) ? -1:result[0]
+   a = sorting(a);
+   let occ = 0, cache=0, sum=0;
 
+   for (let n in a) {sum += a[n]}
+   if (sum/a.length == a[0]) {return -1}
+
+   for (let c=0;c<a.length;c++) {
+      if (a[c] == a[c+1]) {
+         cache = a[c];
+         occ++
+         break;
+      }
+   }
+   
+   // return (result.length < 1 || Object.values(obj)==a.length) ? -1:result[0]
+   return (occ == 0) ? -1:cache;
  }
 
  // TEST CASES
