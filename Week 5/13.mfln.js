d0a3ad7098fcf4a9c2e01e0a762d588dce387864
permaficus <abukhalif2019@gmail.com>
2020-02-05 21:59:40 +0700

    wes males comment cak

--- Week 5/13.mfln.js
+++ Week 5/13.mfln.js
@@ -1,13 +1,23 @@
-function sorting(arrNumber) {
-    return arrNumber.sort();
+const sorting = n => {
+    let f = 0;
+    for (let min=0, max=0, c=0;c<n.length;c++){ if (n[c]>n[c+1]) { min = n[c+1];
+        max = n[c]; n[c]=min; n[c+1]=max; f++}}
+    return (f==0) ? n:sorting(n);
 }
 
 function getTotal(arrNumber) {
-    let highest = Math.max(...arrNumber);
-    arrNumber = arrNumber.reduce((a,b)=> {a[b]=(a[b]||0)+1; return a},{})
+    // Yeep, NO SPREAD SYNTAX, NO .MAP .REDUCE etc etc......... 
+    // let highest = Math.max(...arrNumber);
+    // arrNumber = arrNumber.reduce((a,b)=> {a[b]=(a[b]||0)+1; return a},{})
+    
+    let counter = 0;
+    for (let c=0;c<arrNumber.length;c++) {
+        counter += (arrNumber[c] == arrNumber[arrNumber.length-1]) ? 1:0;
+    }
 
-    return (arrNumber[highest]==undefined) ? '' :
-        `angka paling besar adalah : ${highest} dan jumlah kemunculan sebanyak : ${arrNumber[highest]} kali`;
+    return (arrNumber.length==0) ? '' :
+        `angka paling besar adalah : ${arrNumber[arrNumber.length-1]} dan jumlah kemunculan sebanyak : ${counter} kali`;
+
 }
 
 function mostFrequentLargestNumbers(arrNumber) {
