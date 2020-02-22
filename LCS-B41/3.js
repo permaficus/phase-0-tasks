d218a5255d438e4660a5c17157b0d1daea414c8d
permaficus <abukhalif2019@gmail.com>
2020-02-22 22:04:33 +0700

    Commenting......

--- LCS-B41/3.js
+++ LCS-B41/3.js
@@ -13,28 +13,25 @@ Harus memakai while-loop
 */
 function ticTacToe(array) {
 	//good luck!
-
 	const move = [[0,1,2],[0,3,6],[0,4,8],[2,4,6],[6,7,8],[3,4,5],[1,4,7],[2,6,8]]
 	let [c,x_,o_,board,axis] = [0,0,0,array,0]
 
 	while (c<8) {
-		
 		while (axis < 3) {
 			let x = move[c][axis];
-
+			// check the x and o position based on move guide above
 			if (x >=0 && x <3) { x_ += (board[0][axis] == 'X') ? 1:0; o_ += (board[0][axis] == 'O') ? 1:0 }
 			if (x >=3 && x <6) { x_ += (board[1][axis] == 'X') ? 1:0; o_ += (board[1][axis] == 'O') ? 1:0 }
 			if (x >=6 && x <9) { x_ += (board[2][axis] == 'X') ? 1:0; o_ += (board[2][axis] == 'O') ? 1:0 }
-			
 			axis++
 		}
-		
+		// if x_ or o_ value under 2 point (we need 3), reset counter
 		if (x_ <= 2 && o_ <= 2) {
 			x_ = 0; o_ = 0;
+		// or else break from while-loop (assume we have the winner or draw)
 		} else {
 			break;
 		}
-
 		c++;
 		axis=0;
 	}
