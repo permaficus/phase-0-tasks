d79c2073cdd60441fc2a9308e55e825a7d711249
permaficus <abukhalif2019@gmail.com>
2020-02-23 08:02:56 +0700

    Changing variable name

--- LCS-B41/3.js
+++ LCS-B41/3.js
@@ -14,16 +14,16 @@ Harus memakai while-loop
 function ticTacToe(array) {
 	//good luck!
 	const move = [[0,1,2],[0,3,6],[0,4,8],[2,4,6],[6,7,8],[3,4,5],[1,4,7],[2,6,8]]
-	let [c,x_,o_,board,axis] = [0,0,0,array,0]
+	let [c,x_,o_,board,y] = [0,0,0,array,0]
 
 	while (c<8) {
-		while (axis < 3) {
-			let x = move[c][axis];
+		while (y < 3) {
+			let x = move[c][y];
 			// check the x and o position based on move guide above
-			if (x >=0 && x <3) { x_ += (board[0][axis] == 'X') ? 1:0; o_ += (board[0][axis] == 'O') ? 1:0 }
-			if (x >=3 && x <6) { x_ += (board[1][axis] == 'X') ? 1:0; o_ += (board[1][axis] == 'O') ? 1:0 }
-			if (x >=6 && x <9) { x_ += (board[2][axis] == 'X') ? 1:0; o_ += (board[2][axis] == 'O') ? 1:0 }
-			axis++
+			if (x >=0 && x <3) { x_ += (board[0][y] == 'X') ? 1:0; o_ += (board[0][y] == 'O') ? 1:0 }
+			if (x >=3 && x <6) { x_ += (board[1][y] == 'X') ? 1:0; o_ += (board[1][y] == 'O') ? 1:0 }
+			if (x >=6 && x <9) { x_ += (board[2][y] == 'X') ? 1:0; o_ += (board[2][y] == 'O') ? 1:0 }
+			y++
 		}
 		// if x_ or o_ value under 2 point (we need 3), reset counter
 		if (x_ <= 2 && o_ <= 2) {
@@ -33,7 +33,7 @@ function ticTacToe(array) {
 			break;
 		}
 		c++;
-		axis=0;
+		y=0;
 	}
 
 	return x_ > o_ ? 'Winner : X' : x_ < o_ ? 'Winner : O' : 'Draw!'
